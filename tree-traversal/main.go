2c58787a0bbf4a15a3db7c5a28dbc5e8f4e4aa75
Reid Mason <maddogshain132@gmail.com>
2023-08-09 18:09:44 +0100

    Added pre order search recursive algorithm

--- tree-traversal/main.go
+++ tree-traversal/main.go
@@ -8,9 +8,35 @@ import (
 func main() {
 
 	myTree := node.New("Root",
-		node.New("child1", nil, nil),
+		node.New("child1",
+			node.New("Another child", nil, nil),
+			node.New("More children", nil, nil),
+		),
 		node.New("child2", nil, nil),
 	)
 
-	log.Println(myTree.Left)
+	preOrderSearch(myTree)
+
+}
+
+func walk(current *node.Node, trail []*node.Node) {
+	if current == nil {
+		return
+	}
+
+	trail = append(trail, current)
+
+	log.Printf("Visiting: %s", current.Data)
+	log.Printf("Trail:")
+	for _, x := range trail {
+		log.Printf("  %s", x.Data)
+	}
+
+	walk(current.Left, trail)
+	walk(current.Right, trail)
+}
+
+func preOrderSearch(head *node.Node) {
+	var trail []*node.Node
+	walk(head, trail)
 }

a04773e2125c49dc996a83f3afa3434fa0990d79
Reid Mason <maddogshain132@gmail.com>
2023-08-10 20:13:21 +0100

    Added sqlboiler tests

---
+++ sqlboiler/README.md
@@ -0,0 +1,2 @@
+This seems promising but you are required to install the postgres tools on your system for some reason? \
+Doesn't look like it has support for migrations but that's not the end of the world.
---
+++ sqlboiler/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// db, err := sql.Open("postgres", "user=user password=password dbname=testdb sslmode=disable")
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
+
+	// boil.SetDB(db)
+}

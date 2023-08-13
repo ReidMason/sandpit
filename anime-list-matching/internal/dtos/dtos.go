0f69a4e3b207c739200a2d3795291649405656de
Reid Mason <maddogshain132@gmail.com>
2023-08-13 17:17:46 +0100

    Added anime list request

--- anime-list-matching/internal/dtos/dtos.go
+++ anime-list-matching/internal/dtos/dtos.go
@@ -1,5 +1,46 @@
 package dtos
 
 type AnimeResponse struct {
-	Name string
+	Data struct {
+		Media struct {
+			ID       int      `json:"id"`
+			Format   string   `json:"format"`
+			Episodes int      `json:"episodes"`
+			Synonyms []string `json:"synonyms"`
+			Status   string   `json:"status"`
+			EndDate  struct {
+				Year  int `json:"year"`
+				Month int `json:"month"`
+				Day   int `json:"day"`
+			} `json:"endDate"`
+			StartDate struct {
+				Year  int `json:"year"`
+				Month int `json:"month"`
+				Day   int `json:"day"`
+			} `json:"startDate"`
+			Title struct {
+				English string `json:"english"`
+				Romaji  string `json:"romaji"`
+			} `json:"title"`
+			Relations struct {
+				Edges []struct {
+					RelationType string `json:"relationType"`
+				} `json:"edges"`
+				Nodes []struct {
+					ID      int    `json:"id"`
+					Format  string `json:"format"`
+					EndDate struct {
+						Year  any `json:"year"`
+						Month any `json:"month"`
+						Day   any `json:"day"`
+					} `json:"endDate"`
+					StartDate struct {
+						Year  int `json:"year"`
+						Month int `json:"month"`
+						Day   int `json:"day"`
+					} `json:"startDate"`
+				} `json:"nodes"`
+			} `json:"relations"`
+		} `json:"Media"`
+	} `json:"data"`
 }
--- anime-list-matching/main.go
+++ anime-list-matching/main.go
@@ -4,10 +4,13 @@ import (
 	animeDb "anime-list-matching/internal/animedb"
 	"anime-list-matching/internal/dtos"
 	"anime-list-matching/internal/migrations"
+	"bytes"
 	"context"
 	"database/sql"
 	"encoding/json"
+	"io"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -22,15 +25,15 @@ func main() {
 	ctx := context.Background()
 	queries := animeDb.New(db)
 
-	thing := dtos.AnimeResponse{
-		Name: "testing",
+	animeResult := getAnime(113415)
+	byteResult, err := json.Marshal(animeResult)
+	if err != nil {
+		log.Print(err)
 	}
-	thingJson, err := json.Marshal(thing)
-	log.Println(thingJson)
 
 	_, err = queries.CacheAnimeResult(ctx, animeDb.CacheAnimeResultParams{
 		ID:       1,
-		Response: thingJson,
+		Response: byteResult,
 	})
 	if err != nil {
 		log.Println(err)
@@ -47,4 +50,97 @@ func main() {
 	}
 
 	log.Println(data)
+}
+
+type GraphqlBody[T any] struct {
+	Query     string `json:"query"`
+	Variables T      `json:"variables"`
+}
+
+type GetAnimeVariables struct {
+	AnimeId int `json:"anime_id"`
+}
+
+func getAnime(animeId int) dtos.AnimeResponse {
+	url := "https://graphql.anilist.co/"
+
+	query := `query ($anime_id: Int) {
+    Media(id: $anime_id, type: ANIME) {
+      id
+      format
+      episodes
+      synonyms
+      status
+      endDate {
+        year
+        month
+        day
+        }
+      startDate {
+        year
+        month
+        day
+        }
+      title {
+        english
+        romaji
+        }
+      relations {
+        edges {
+          relationType
+            }
+        nodes {
+          id
+          format
+          endDate {
+            year
+            month
+            day
+                }
+            startDate {
+                year
+                month
+                day
+                }
+            }
+        }
+    }
+}`
+
+	body := GraphqlBody[GetAnimeVariables]{
+		Query: query,
+		Variables: GetAnimeVariables{
+			AnimeId: animeId,
+		},
+	}
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		log.Print(err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data dtos.AnimeResponse
+	err = json.Unmarshal(jsonData, &data)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return data
 }

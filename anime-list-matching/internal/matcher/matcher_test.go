542970e245daf9c229b0d99f03c50ce1182420f5
Reid Mason <maddogshain132@gmail.com>
2023-08-17 19:09:33 +0100

    Added more tests for simple matching

--- anime-list-matching/internal/matcher/matcher_test.go
+++ anime-list-matching/internal/matcher/matcher_test.go
@@ -13,14 +13,28 @@ import (
 
 func TestMatchExactEpisodeMatch(t *testing.T) {
 	tests := []struct {
+		title           string
 		startingAnimeId int32
 		targetEps       int
 		expectedIds     []int
 	}{
 		{
+			"Attack on titan",
 			16498,
 			88,
 			[]int{16498, 20958, 99147, 104578, 110277, 131681, 146984},
+		},
+		{
+			"Overlord",
+			20832,
+			52,
+			[]int{20832, 98437, 101474, 133844},
+		},
+		{
+			"JoJo's Bizarre Adventure",
+			14719,
+			190,
+			[]int{14719, 20474, 20799, 21450, 102883, 131942, 146722},
 		},
 	}
 
@@ -37,7 +51,7 @@ func TestMatchExactEpisodeMatch(t *testing.T) {
 		test := test
 		res, err := MatchAnime(test.startingAnimeId, make([]anilist.Anime, 0), test.targetEps, queries, ctx)
 		if err != nil {
-			t.Errorf("Failed to find match")
+			t.Errorf("Failed to find match for '%s'", test.title)
 		}
 
 		for i, r := range res {

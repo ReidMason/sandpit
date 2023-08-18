45d49623b2b97af4953b36d38eb2c0defe090ab5
Reid Mason <maddogshain132@gmail.com>
2023-08-18 20:36:07 +0100

    Added failing test for incomplete anilist season

--- anime-list-matching/internal/matcher/matcher_test.go
+++ anime-list-matching/internal/matcher/matcher_test.go
@@ -62,3 +62,43 @@ func TestMatchExactEpisodeMatch(t *testing.T) {
 		}
 	}
 }
+
+func TestMatchExactEpisodeMatchWithIncompleteSeason(t *testing.T) {
+	tests := []struct {
+		title           string
+		startingAnimeId int32
+		targetEps       int
+		expectedIds     []int
+	}{
+		{
+			"JoJo's Bizarre Adventure",
+			14719,
+			176,
+			[]int{14719, 20474, 20799, 21450, 102883, 131942, 146722},
+		},
+	}
+
+	connectionString := "postgres://user:password@localhost:5432/postgres?sslmode=disable"
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Panic("Failed to connect to database", err)
+	}
+
+	ctx := context.Background()
+	queries := animeDb.New(db)
+
+	for _, test := range tests {
+		test := test
+		res, err := MatchAnime(test.startingAnimeId, make([]anilist.Anime, 0), test.targetEps, queries, ctx)
+		if err != nil {
+			t.Errorf("Failed to find match for '%s'", test.title)
+		}
+
+		for i, r := range res {
+			expected := test.expectedIds[i]
+			if r.ID != expected {
+				t.Errorf("Wrong anime id found. Expected: '%d' found '%d'", expected, r.ID)
+			}
+		}
+	}
+}

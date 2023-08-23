20345a7add5b68f41100bbf659c9db075d3610a0
Reid Mason <maddogshain132@gmail.com>
2023-08-23 22:33:04 +0100

    Refactored golang endpoints

--- htmx-todolist/main.go
+++ htmx-todolist/main.go
@@ -14,34 +14,36 @@ type Todo struct {
 }
 
 func main() {
-	h1 := func(w http.ResponseWriter, _ *http.Request) {
-		templ := template.Must(template.ParseFiles("index.html"))
-		templ.Execute(w, nil)
-	}
-	http.HandleFunc("/", h1)
+	http.HandleFunc("/", getIndex)
 
-	getTodos := func(w http.ResponseWriter, _ *http.Request) {
-		todos := map[string][]Todo{
-			"Todos": {
-				{
-					id:        1,
-					userId:    1,
-					Title:     "delectus aut autem",
-					completed: false,
-				},
-				{
-					id:        2,
-					userId:    1,
-					Title:     "quis ut nam facilis et officia qui",
-					completed: false,
-				},
-			},
-		}
-
-		templ := template.Must(template.ParseFiles("templates/todos.html"))
-		templ.Execute(w, todos)
-	}
 	http.HandleFunc("/todos", getTodos)
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func getIndex(w http.ResponseWriter, _ *http.Request) {
+	templ := template.Must(template.ParseFiles("index.html"))
+	templ.Execute(w, nil)
+}
+
+func getTodos(w http.ResponseWriter, _ *http.Request) {
+	todos := map[string][]Todo{
+		"Todos": {
+			{
+				id:        1,
+				userId:    1,
+				Title:     "delectus aut autem",
+				completed: false,
+			},
+			{
+				id:        2,
+				userId:    1,
+				Title:     "quis ut nam facilis et officia qui",
+				completed: false,
+			},
+		},
+	}
+
+	templ := template.Must(template.ParseFiles("templates/todos.html"))
+	templ.Execute(w, todos)
 }

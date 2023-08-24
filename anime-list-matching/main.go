5d7a04e38763c84a9e0df9ca970e45d4dae6d9d6
Reid Mason <maddogshain132@gmail.com>
2023-08-24 20:23:27 +0100

    Added environment variables for storing Plex token

--- anime-list-matching/main.go
+++ anime-list-matching/main.go
@@ -1,33 +1,44 @@
 package main
 
 import (
-	"anime-list-matching/internal/anilist"
-	"anime-list-matching/internal/animeDb"
-	"anime-list-matching/internal/matcher"
-	"anime-list-matching/internal/migrations"
-	"context"
-	"database/sql"
+	"anime-list-matching/internal/plex"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	connectionString := "postgres://user:password@localhost:5432/postgres?sslmode=disable"
-	db, err := sql.Open("postgres", connectionString)
+	err := godotenv.Load()
 	if err != nil {
-		log.Panic("Failed to connect to database", err)
+		log.Fatal("Error loading .env file")
 	}
 
-	migrations.ApplyMigrations(db)
+	token := os.Getenv("PLEX_TOKEN")
+	series := plex.GetSeries(token)
 
-	ctx := context.Background()
-	queries := animeDb.New(db)
-
-	targetEps := 88
-	path, err := matcher.MatchAnime(16498, make([]anilist.Anime, 0), targetEps, queries, ctx)
-	if err != nil {
-		log.Fatal(err)
+	for _, series := range series.MediaContainer.Metadata {
+		log.Println(series.Title, series.SeasonCount)
 	}
 
-	log.Print("Done mathcing anime found:")
-	matcher.PrintTraversalPath(path)
+	// connectionString := "postgres://user:password@localhost:5432/postgres?sslmode=disable"
+	// db, err := sql.Open("postgres", connectionString)
+	// if err != nil {
+	// 	log.Panic("Failed to connect to database", err)
+	// }
+	//
+	// migrations.ApplyMigrations(db)
+	//
+	// ctx := context.Background()
+	// queries := animeDb.New(db)
+	//
+	// targetEps := 88
+	// path, err := matcher.MatchAnime(16498, make([]anilist.Anime, 0), targetEps, queries, ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// log.Print("Done mathcing anime found:")
+	// matcher.PrintTraversalPath(path)
+
 }

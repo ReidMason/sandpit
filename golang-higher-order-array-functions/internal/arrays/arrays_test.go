a1d880939a3f43a807b6fa103e962f2ce2c22d09
Reid Mason <maddogshain132@gmail.com>
2023-08-27 19:02:11 +0100

    Added filter tests for objects

--- golang-higher-order-array-functions/internal/arrays/arrays_test.go
+++ golang-higher-order-array-functions/internal/arrays/arrays_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestFilter(t *testing.T) {
+func TestFilterInts(t *testing.T) {
 	tests := []struct {
 		filter   func(x int32) bool
 		input    []int32
@@ -44,6 +44,55 @@ func TestFilter(t *testing.T) {
 			result := res[i]
 			if expected != result {
 				t.Errorf("Wrong element found. Expected: '%d' found '%d'", result, expected)
+			}
+		}
+	}
+}
+
+func TestFilterObjects(t *testing.T) {
+	type Book struct {
+		title string
+	}
+
+	tests := []struct {
+		filter   func(x Book) bool
+		input    []Book
+		expected []Book
+	}{
+		{
+			func(x Book) bool { return x.title != "book1" },
+			[]Book{{title: "book1"}, {title: "book2"}, {title: "book3"}},
+			[]Book{{title: "book2"}, {title: "book3"}},
+		},
+		{
+			func(x Book) bool { return x != Book{title: "book1"} },
+			[]Book{{title: "book1"}},
+			[]Book{},
+		},
+		{
+			func(x Book) bool { return x.title != "" },
+			[]Book{{title: "book1"}, {title: "book2"}, {title: "book3"}},
+			[]Book{{title: "book1"}, {title: "book2"}, {title: "book3"}},
+		},
+		{
+			func(x Book) bool { return x.title != "" },
+			[]Book{},
+			[]Book{},
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		res := Filter(test.input, test.filter)
+
+		if len(res) != len(test.expected) {
+			t.Errorf("Wrong number of elements in array. Expected: %d found: %d", len(test.expected), len(res))
+		}
+
+		for i, expected := range test.expected {
+			result := res[i]
+			if expected != result {
+				t.Errorf("Wrong element found. Expected: '%s' found '%s'", result, expected)
 			}
 		}
 	}

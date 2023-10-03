66f7a928c2b7db3b66751ba4ab24333d836f1ee9
Reid Mason <maddogshain132@gmail.com>
2023-10-03 11:06:31 +0100

    Added basic git diff parsing

--- git-ui/internal/git/git.go
+++ git-ui/internal/git/git.go
@@ -5,10 +5,86 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+	"unicode/utf8"
 )
 
-func getDiff() string {
-	cmd := exec.Command("git", "diff", "../../testfile.txt")
+type Diff struct {
+	Diff1 []DiffLine
+	Diff2 []DiffLine
+}
+
+type DiffLine struct {
+	Content string
+}
+
+func GetDiff(diffString string) Diff {
+	lines := strings.Split(diffString, "\n")
+
+	diff := Diff{Diff1: make([]DiffLine, 0), Diff2: make([]DiffLine, 0)}
+
+	start := false
+	removals := 0
+	additions := 0
+	diffLineBlank := DiffLine{Content: ""}
+	for _, line := range lines {
+		if strings.HasPrefix(line, "@@") && strings.HasSuffix(line, "@@") {
+			start = true
+			continue
+		}
+
+		if !start {
+			continue
+		}
+
+		letter, lineString := trimFirstRune(line)
+		diffLine := DiffLine{Content: lineString}
+
+		if letter == '-' {
+			removals++
+			diff.Diff1 = append(diff.Diff1, diffLine)
+		} else if letter == '+' {
+			diff.Diff2 = append(diff.Diff2, diffLine)
+			if removals > 0 {
+				removals--
+			}
+
+			if removals == 0 {
+				additions++
+			}
+		} else {
+			for i := 0; i < removals; i++ {
+				diff.Diff2 = append(diff.Diff2, diffLineBlank)
+			}
+			removals = 0
+
+			for i := 0; i < additions; i++ {
+				diff.Diff1 = append(diff.Diff1, diffLineBlank)
+			}
+			additions = 0
+
+			diff.Diff1 = append(diff.Diff1, diffLine)
+			diff.Diff2 = append(diff.Diff2, diffLine)
+		}
+	}
+
+	for i := 0; i < removals; i++ {
+		diff.Diff2 = append(diff.Diff2, diffLineBlank)
+	}
+
+	for i := 0; i < additions; i++ {
+		diff.Diff1 = append(diff.Diff1, diffLineBlank)
+	}
+
+	return diff
+}
+
+func trimFirstRune(s string) (rune, string) {
+	r, i := utf8.DecodeRuneInString(s)
+	return r, s[i:]
+}
+
+func GetRawDiff() string {
+	cmd := exec.Command("git", "diff", "--no-prefix", "-U1000", "testfile.txt")
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
@@ -18,5 +94,5 @@ func getDiff() string {
 		log.Fatal("Failed to get git diff")
 	}
 
-	return strings.TrimSpace(out.String())
+	return out.String()
 }
--- git-ui/internal/git/git_test.go
+++ git-ui/internal/git/git_test.go
@@ -1,14 +1,41 @@
 package git
 
 import (
-	"strings"
 	"testing"
 )
 
-func TestGetDiff(t *testing.T) {
-	result := getDiff()
+// func TestGetRawDiff(t *testing.T) {
+// 	result := GetRawDiff()
+//
+// 	expected := `diff --git git-ui/testfile.txt git-ui/testfile.txt
+// index 35b5809..4492ac6 100644
+// --- git-ui/testfile.txt
+// +++ git-ui/testfile.txt
+// @@ -1,4 +1,5 @@
+//  This is a test file
+// -
+//  These lines are committed now
+// -I have added some more content
+// +I have added this is a change more content
+// +
+// +This is a new thing`
+//
+// 	resultLines := strings.Split(result, "\n")
+// 	for i, line := range strings.Split(expected, "\n") {
+// 		resLine := resultLines[i]
+//
+// 		if line != resLine {
+// 			t.Fatalf("Line match failed. Expected: '%s' Got: '%s'", line, resLine)
+// 		}
+// 	}
+//
+// 	if len(result) != len(expected) {
+// 		t.Fatalf("Length difference. Expected: %d Got: %d", len(expected), len(result))
+// 	}
+// }
 
-	expected := `diff --git a/git-ui/testfile.txt b/git-ui/testfile.txt
+func TestGetDiff(t *testing.T) {
+	rawDiff := `diff --git a/git-ui/testfile.txt b/git-ui/testfile.txt
 index 35b5809..4492ac6 100644
 --- a/git-ui/testfile.txt
 +++ b/git-ui/testfile.txt
@@ -21,16 +48,48 @@ index 35b5809..4492ac6 100644
 +
 +This is a new thing`
 
-	resultLines := strings.Split(result, "\n")
-	for i, line := range strings.Split(expected, "\n") {
-		resLine := resultLines[i]
+	result := GetDiff(rawDiff)
 
-		if line != resLine {
-			t.Fatalf("Line matche failed. Expected: '%s' Got: '%s'", line, resLine)
+	expected := Diff{
+		Diff1: []DiffLine{
+			{Content: "This is a test file"},
+			{Content: ""},
+			{Content: "These lines are committed now"},
+			{Content: "I have added some more content"},
+			{Content: ""},
+			{Content: ""},
+		},
+		Diff2: []DiffLine{
+			{Content: "This is a test file"},
+			{Content: ""},
+			{Content: "These lines are committed now"},
+			{Content: "I have added this is a change more content"},
+			{Content: ""},
+			{Content: "This is a new thing"},
+		},
+	}
+
+	t.Log("----- Start diff1")
+	for i, expectedDiffLine := range expected.Diff1 {
+		resultDiffLine := result.Diff1[i]
+		t.Log(resultDiffLine.Content)
+
+		if expectedDiffLine.Content != resultDiffLine.Content {
+			t.Fatalf("Diff1 failed line %d.\nExpected: '%s'\n     Got: '%s'", i+1, expectedDiffLine.Content, resultDiffLine.Content)
 		}
 	}
 
-	if len(result) != len(expected) {
-		t.Fatalf("Length difference. Expected: %d Got: %d", len(expected), len(result))
+	t.Log("----- Start diff2")
+	for i, expectedDiffLine := range expected.Diff2 {
+		resultDiffLine := result.Diff2[i]
+		t.Log(resultDiffLine.Content)
+
+		if expectedDiffLine.Content != resultDiffLine.Content {
+			t.Fatalf("Diff2 failed line %d.\nExpected: '%s'\n     Got: '%s'", i+1, expectedDiffLine.Content, resultDiffLine.Content)
+		}
 	}
+
+	// if !reflect.DeepEqual(result, expected) {
+	// 	t.Fatal("Expected diff length doesn't match")
+	// }
 }

f4301851a26a8e3991d6e8aca75a764738e5116d
Reid Mason <maddogshain132@gmail.com>
2023-11-20 23:45:41 +0000

    Added htmx trigger

--- htmx-testing/main.go
+++ htmx-testing/main.go
@@ -12,6 +12,7 @@ func main() {
 	http.HandleFunc("/", getIndex)
 
 	http.HandleFunc("/setTheme1", setTheme1)
+	http.HandleFunc("/getThing", getThing)
 	// http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
 	// 	serveWs(w, r)
 	// })
@@ -21,6 +22,12 @@ func main() {
 
 func setTheme1(w http.ResponseWriter, _ *http.Request) {
 	classes = "bg-blue-900 rounded p-4"
+	w.Header().Set("HX-Trigger", "theme-update")
+	w.WriteHeader(http.StatusOK)
+}
+
+func getThing(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("<div>Hello!</div>"))
 }
 
 func getIndex(w http.ResponseWriter, _ *http.Request) {

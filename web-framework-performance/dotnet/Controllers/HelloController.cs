48e09ec9f52c39fb1b1cdf8e740671e6b2c8d186
Reid Mason <maddogshain132@gmail.com>
2024-01-28 00:11:40 +0000

    Added dotnet webapi to web framework benchmarks

---
+++ web-framework-performance/dotnet/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class HelloController : ControllerBase
+{
+    private readonly ILogger<HelloController> _logger;
+
+    public HelloController(ILogger<HelloController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet()]
+    public IActionResult Get()
+    {
+      return Ok("Hello World!");
+    }
+}

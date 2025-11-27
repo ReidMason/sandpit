cdabb62b43bd01a0e677dda5b716faec3528d13d
Reid Mason <maddogshain132@gmail.com>
2025-11-27 23:38:35 +0000

    feat: added keymap initialisation

--- learning-godot/scripts/domain/Keys.cs
+++ learning-godot/scripts/domain/Keys.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace scripts.domain;
@@ -12,15 +13,36 @@ public static class Keys
 	
 	private static bool InputMapInitialised { get; set; } = false;
 	
+	private static readonly Dictionary<string, Key[]> ActionKeyMap = new()
+	{
+		{ Left, new[] { Key.A, Key.Left } },
+		{ Right, new[] { Key.D, Key.Right } },
+		{ Up, new[] { Key.W, Key.Up } },
+		{ Down, new[] { Key.S, Key.Down } }
+	};
+	
 	public static void SetupInputMap()
 	{
 		if (InputMapInitialised) return;
 		
-		var keyEventA = new InputEventKey();
-		keyEventA.Keycode = Key.A;
-		InputMap.AddAction(Left);
-		InputMap.ActionAddEvent(Left, keyEventA);
-		
+		foreach (var (action, keys) in ActionKeyMap)
+			AddKeysToActionKeyMap(action, keys);
+			
 		InputMapInitialised = true;
+	}
+	
+	private static void AddKeysToActionKeyMap(string action, Key[] keys)
+	{
+		InputMap.AddAction(action);
+			
+		foreach (var key in keys)
+			AddKeyToActionKeyMap(action, key);
+	}
+	
+	private static void AddKeyToActionKeyMap(string action, Key key)
+	{
+		var keyEvent = new InputEventKey();
+		keyEvent.Keycode = key;
+		InputMap.ActionAddEvent(action, keyEvent);	
 	}
 }
